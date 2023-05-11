ab171a99a96ca6eb4b0c8de637ab6cbe6bab7db4
Shreya Sarker <shreya.sarker@gmail.com>
2023-05-11 17:21:57 +0600

    Frontend: Added Category Page

---
+++ tecalley-frontend/app/category/[slug]/page.js
@@ -0,0 +1,29 @@
+import { Product, Wrapper } from '@/components';
+
+const Category = ({ params }) => {
+  return (
+    <div className="w-full md:py-20">
+      <Wrapper>
+        <div className="text-center max-w-[850px] mx-auto mb-14">
+          <div className="text-[28px] md:text-[34px] font-semibold leading-tight">
+            Category: <span className="capitalize">{params.slug}</span>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-4 px-5 md:px-0">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </Wrapper>
+    </div>
+  )
+}
+
+export default Category
