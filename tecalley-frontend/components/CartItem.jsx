1541674317bc71b96a2583537b862bd11847b8d9
Shreya Sarker <shreya.sarker@gmail.com>
2023-05-17 20:54:22 +0600

    Feat Frontend: WIP: Cart page populated with Data

--- tecalley-frontend/components/CartItem.jsx
+++ tecalley-frontend/components/CartItem.jsx
@@ -1,30 +1,30 @@
 import { RiDeleteBin6Line } from 'react-icons/ri';
 
-const CartItem = () => {
+const CartItem = ({ cartItem }) => {
   return (
     <div className="flex py-5 gap-3 md:gap-5 border-b">
       <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
-        <img src="/assets/products/samsung-s23-ultra/S23U-V2-Configurator-DT-800x600.webp" alt="Product" />
+        <img src={cartItem.attributes.thumbnail.data.attributes.url} alt={cartItem.attributes.name} className="w-[120px] h-[120px]" />
       </div>
 
       <div className="w-full flex flex-col">
         <div className="flex flex-col md:flex-row justify-between">
           <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
-            Samsung S23 Ultra
+            {cartItem.attributes.name}
           </div>
 
           <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
-            128 GB | 256 GB | 1 TB
+            {cartItem.attributes.subtitle}
           </div>
 
           <div className="text-sm md:text-md font-bold text-black/[0.5] mt-2">
-            Price: $1439.99
+            Price: ${cartItem.attributes.price}
           </div>
 
         </div>
 
         <div className="text-md font-medium text-black/[0.5] hidden md:block">
-          128 GB | 256 GB | 1 TB
+          {cartItem.attributes.subtitle}
         </div>
 
         <div className="flex items-center justify-between mt-4">
@@ -32,17 +32,10 @@ const CartItem = () => {
 
             <div className="flex items-center gap-1">
               <div className="font-semibold">Quantity:</div>
-              <select className="hover:text-black">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+              <select className="hover:text-black" defaultValue={cartItem.quantity}>
+                {Array.from({length: 10}, (_, i) => (
+                  <option value={i + 1} key={i + 1}>{i + 1}</option>
+                ))}
               </select>
             </div>
 
